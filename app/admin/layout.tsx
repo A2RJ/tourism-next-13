6f9b542a70fee3cc1990316b08bf338edd14dec3
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2023-06-20 16:57:01 +0800

    Refactoring admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,17 @@
-import { Children } from "../layout";
+import { Sidebar } from "@/components/ui/music/sidebar";
+import { Children } from "@/types/layout";
 
 export default function Layout({ children }: Children) {
-    return children;
+  return (
+    <div className="hidden md:block container">
+      <div className="border-t">
+        <div className="bg-background">
+          <div className="grid lg:grid-cols-5">
+            <Sidebar playlists={[]} className="hidden lg:block" />
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
